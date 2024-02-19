6bddaf9d376e7239994cc62aa14c08b0dded5a56
amam <angelina.malmros@sbab.se>
2024-02-19 10:30:49 +0100

    formatting

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,45 +1,45 @@
 import api from './api';
 
 export async function registerUser(
-    firstName: string,
-    lastName: string,
-    email: string,
-    password: string
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string
 ) {
-    try {
-        return await api.post(
-            '/auth/register',
-            {
-                firstName: firstName,
-                lastName: lastName,
-                email: email,
-                password: password
-            }
-        );
-    } catch (error) {
-        console.error(error);
-        return error;
-    }
+  try {
+    return await api.post(
+      '/auth/register',
+      {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 }
 
 export async function logIn(
-    email: string,
-    password: string
+  email: string,
+  password: string
 ) {
-    try {
-        const response = await api.post(
-            '/auth/authenticate',
-            {
-                email: email,
-                password: password
-            }
-        );
+  try {
+    const response = await api.post(
+      '/auth/authenticate',
+      {
+        email: email,
+        password: password
+      }
+    );
 
-        if (response.status == 200) {
-            sessionStorage.setItem('isAuthenticated', 'true'),
-            sessionStorage.setItem('token', response.data.token);
-        } 
-    } catch (error) {
-        console.error(error);
-    }
+    if (response.status == 200) {
+      sessionStorage.setItem('isAuthenticated', 'true'),
+      sessionStorage.setItem('token', response.data.token);
+    } 
+  } catch (error) {
+    console.error(error);
+  }
 }
--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,58 +1,59 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm, useFormContext, FieldValues } from 'react-hook-form';
+import { useForm, FieldValues } from 'react-hook-form';
 import { z } from 'zod';
 import { registerUser } from '../../api';
 import { useState } from 'react';
+import { useFormContext } from '../../context/RegisterFormContext';
 
 const schema = z.object({
-    firstName: z.string().min(1, 'First name is required.'),
-    lastName: z.string().min(1, 'Last name is required.'),
-    email: z.string().min(5).email('Please provide a valid email address.'),
-    password: z.string().min(8, {message: "Password must be at least 8 characters."})
-    .regex(/\d/, {message: "Password must contain at least one digit [0-9]."})
-    .regex(/[A-Z]/, {message: "Password must contain at least one uppercase letter [A-Z]"})
-    .regex(/[a-z]/, {message: "Password must contain at least one lowercase letter [a-z]"}),
-    confirmPassword: z.string()
+  firstName: z.string().min(1, 'First name is required.'),
+  lastName: z.string().min(1, 'Last name is required.'),
+  email: z.string().min(5).email('Please provide a valid email address.'),
+  password: z.string().min(8, { message: 'Password must be at least 8 characters.' })
+    .regex(/\d/, { message: 'Password must contain at least one digit [0-9].' })
+    .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter [A-Z]' })
+    .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter [a-z]' }),
+  confirmPassword: z.string()
 }).refine(value => value.password == value.confirmPassword, {
-    message: "Password doesn't match. Try again.",
-    path: ["confirmPassword"]
+  message: 'Password doesn\'t match. Try again.',
+  path: [ 'confirmPassword' ]
 });
 
 type FormData = z.infer<typeof schema>;
 
 export function RegisterForm() {
-    const {
-        register,
-        handleSubmit,
-        formState: {errors}
-    } = useForm<FormData>({
-        resolver: zodResolver(schema)
-    });
-    const {formData} = useFormContext();
-    const [username, setUsername] = useState<string>("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  });
+  const { formData } = useFormContext();
+  const [ username, setUsername ] = useState<string>('');
 
-    async function onSubmit(data: FieldValues) {
-        try {
-            const response = await registerUser(
-                formData.firstName,
-                formData.lastName,
-                data.email,
-                data.password
-            );
+  async function onSubmit(data: FieldValues) {
+    try {
+      const response = await registerUser(
+        formData.firstName,
+        formData.lastName,
+        data.email,
+        data.password
+      );
 
-            if (response == 200 || response == 201) {
-                setUsername(data.email);
-            } else {
-                // method for handling user already exists
-            }
-        } catch (error) {
-            console.error(error);
-        }
+      if (response == 200 || response == 201) {
+        setUsername(data.email);
+      } else {
+        // method for handling user already exists
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    return (
-        <>
+  return (
+    <>
         // form
-        </>
-    )
+    </>
+  );
 }
