4b2980ed23f2a41a2ad5da5c7482f5ee1e92a98a
amam <angelina.malmros@sbab.se>
2024-02-21 13:56:03 +0100

    WIP routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,28 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
-import { LoginView } from './components/views/LoginView';
-import { LandingPage } from './components/views/LandingPage';
-import { RegisterView } from './components/views/RegisterView';
+import { LoginView } from './views/LoginView';
+import { LandingPage } from './views/LandingPage';
+import { RegisterView } from './views/RegisterView';
+import { MyPages } from './views/MyPages';
 
 function App() {
     const { isAuthenticated } = useContext(AuthContext);  
 
     return (
+        // can I use sidebar here to get it across all pages?
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<LandingPage />} />
                 <Route path='/login' element={<LoginView />} />
                 <Route path="/register" element={<RegisterView/>}/>
                 { isAuthenticated && 
-        <>
-          // TODO add routes for authenticated views (my pages)
-        </>
+                <>
+                    <Route path='/mypages' element={<MyPages />} />
+                    // archive 
+                    // new dream
+                    // settings (update password/email)
+                </>
                 }
             </Routes>
         </BrowserRouter>
