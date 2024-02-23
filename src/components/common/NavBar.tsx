002f55b55a3215b0fcc30d4633f5603f66c5fc7a
amam <angelina.malmros@sbab.se>
2024-02-23 14:08:00 +0100

    navbar

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,13 +1,42 @@
-import { Button } from '@mui/material';
-
+import { useContext } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { GiDreamCatcher } from 'react-icons/gi';
+import { AuthContext } from '../../context/AuthContext';
+import '../../App.css';
 
 export function NavBar() {
+    const { username, isAuthenticated } = useContext(AuthContext);
 
     return (
-        <>
-            <div className="min-vw-100" style={{ backgroundColor: 'white', height: '50px' }}>
-                <Button>my pages</Button>
-            </div>
-        </>
+        <Navbar bg="navColor" variant="dark" expand="lg" sticky='top'>
+            <Container>
+                <Navbar.Brand href="/"><GiDreamCatcher /> dream tracker</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    {!isAuthenticated && (
+                        <>
+                            <Nav className="me-auto">
+                                <Nav.Link href="/register">register</Nav.Link>
+                                <Nav.Link href="/login">log in</Nav.Link>
+                            </Nav>
+                        </>
+                    )}
+                    {isAuthenticated && (
+                        <>
+                            <Nav className="me-auto">
+                                <Nav.Link href="/mypages">dashboard</Nav.Link>
+                                <Nav.Link href="/newdream">new dream</Nav.Link>
+                                <Nav.Link href="/archive">archive</Nav.Link>
+                                <Nav.Link href="/settings">settings</Nav.Link>
+                            </Nav>
+                            
+                            <Navbar.Text>signed in as <span className='fw-bold'>{username}</span></Navbar.Text>
+                        </>
+                    )}
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
--- src/views/MyPages.tsx
+++ src/views/MyPages.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import styled from 'styled-components';
 
 export function MyPages() {
     const { token, userId, username } = useContext(AuthContext);
@@ -8,11 +9,17 @@ export function MyPages() {
 
     return (
         <>
-            <div>
-                <h1>
-                    welcome {username}
-                </h1>
-            </div>
+            <StyledDiv>
+                <h1> welcome {username}</h1>
+            </StyledDiv>
         </>
     );
 }
+
+const StyledDiv = styled.div`
+    
+    background-color: rgba(250, 250, 250, 0.8);
+    padding: 50px;
+    margin: 100px;
+    color: #000000; 
+`;
