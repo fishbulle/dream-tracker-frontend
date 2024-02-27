5d3641a1aed718350be4818d10dcd2bc0900871c
amam <linaemalmros@gmail.com>
2024-02-27 16:24:55 +0100

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { Background } from './components/common/Background';
-import { NavBar } from './components/common/NavBar';
-import { ApplicationRoutes } from './routes/ApplicationRoutes';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Background } from "./components/common/Background";
+import { NavBar } from "./components/common/NavBar";
+import { ApplicationRoutes } from "./routes/ApplicationRoutes";
 
-function App() {  
-
-    return (
-        <>
-            <Background>
-                <NavBar />
-                <ApplicationRoutes />
-            </Background>
-        </>
-    );
+function App() {
+  return (
+    <>
+      <Background>
+        <NavBar />
+        <ApplicationRoutes />
+      </Background>
+    </>
+  );
 }
 
 export default App;
