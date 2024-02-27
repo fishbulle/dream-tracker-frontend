df1ebb6f3cdb6fa158928fdd6d5180711703ce19
amam <linaemalmros@gmail.com>
2024-02-27 09:05:06 +0100

    styling

--- src/components/common/Background.tsx
+++ src/components/common/Background.tsx
@@ -1,5 +1,5 @@
 import { Container } from 'react-bootstrap';
-import sky from '../../assets/nightsky.jpg';
+import purplesky from '../../assets/purplesky.jpg';
 
 interface IProps {
     children: React.ReactNode
@@ -8,7 +8,7 @@ interface IProps {
 export function Background({ children }: IProps) {
 
     return (
-        <Container style={{ backgroundImage: `url(${sky})`, backgroundSize: 'cover' }} className='min-vw-100 min-vh-100 p-0'>
+        <Container style={{ backgroundImage: `url(${purplesky})`, backgroundSize: 'cover' }} className='min-vw-100 min-vh-100 p-0'>
             {children}
         </Container>
     );
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
     background-color: rgba(250, 250, 250, 0.8);
-    padding: 20px;
+    padding: 50px;
     margin: 100px;
     color: #000000;
     border-radius: 10px;
@@ -11,20 +13,16 @@ export const StyledDiv = styled.div`
 export const ButtonDiv = styled.div`
     display: flex;
     justify-content: center;
-    padding: 30px;
+    padding-top: 20px;
 `;
 
 export const StyledButton = styled.button`
-    background-color: #742898;
+    background-color: #79155B;
     color: #ffffff;
     margin: 10px;
-    box-shadow: none;
-    &:focus {
-        outline-color: none !important;
-        box-shadow: none !important;
-    }
     &:hover {
-        color: #000000;
+        background-color: #C23373;
+        box-shadow: 2px 2px 5px black;
     }
 `;
 
