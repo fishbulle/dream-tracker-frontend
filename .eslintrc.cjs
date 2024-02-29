a902f95393e8775a813ea9beb45f431f088c5977
amam <linaemalmros@gmail.com>
2024-02-29 14:30:04 +0100

    config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,25 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2020: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier/recommended',
-        'prettier',
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  ignorePatterns: ['dist'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier', 'react-refresh'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    overrides: [],
-    ignorePatterns: ['dist'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier', 'react-refresh'],
-    rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true },
-        ],
-    },
+  },
 };
