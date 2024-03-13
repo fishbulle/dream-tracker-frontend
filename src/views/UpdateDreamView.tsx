8015f2eecce13a6bf5e2333cb1621461eeda6b36
amam <linaemalmros@gmail.com>
2024-03-13 12:03:20 +0100

    Rename view

---
+++ src/views/UpdateDreamView.tsx
@@ -0,0 +1,12 @@
+import { StyledWrapper } from '../styles/styles';
+import { UpdateDreamForm } from '../components/forms/UpdateDreamForm';
+
+export function UpdateDream() {
+  return (
+    <>
+      <StyledWrapper>
+        <UpdateDreamForm />
+      </StyledWrapper>
+    </>
+  );
+}
